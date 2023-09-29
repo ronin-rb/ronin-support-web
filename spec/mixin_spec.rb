c4b8ade37e1e910b9df0dbc7024768742b923067
Postmodern <postmodern.mod3@gmail.com>
2023-09-29 01:29:50 -0700

    Added `Ronin::Support::Web::Mixin` (closes #3).

---
+++ spec/mixin_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'ronin/support/web/mixin'
+
+describe Ronin::Support::Web::Mixin do
+  subject do
+    Class.new { include Ronin::Support::Web::Mixin }
+  end
+
+  it "must include `Ronin::Support::Web::HTML::Mixin`" do
+    expect(subject).to include(Ronin::Support::Web::HTML::Mixin)
+  end
+
+  it "must include `Ronin::Support::Web::XML::Mixin`" do
+    expect(subject).to include(Ronin::Support::Web::XML::Mixin)
+  end
+end
